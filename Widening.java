ef04ee5575101e994927b92dd4898093f21de83c
Piyu1913 <109063346+Piyu1913@users.noreply.github.com>
2022-07-15 14:37:56 +0530

    Add files via upload

---
+++ Widening.java
@@ -0,0 +1,11 @@
+class Widening
+{
+    public static void main(String[]a)
+    {
+       int myInt=10;
+       double myDouble = myInt;
+       System.out.println(myInt);
+       System.out.println(myDouble);
+    }
+}
+      
