4272cec3eecccd800af2c322364c219b6e9c34b2
Piyu1913 <109063346+Piyu1913@users.noreply.github.com>
2022-07-19 16:44:55 +0530

    Add files via upload

---
+++ AirthmeticOperator.java
@@ -0,0 +1,18 @@
+class AirthmeticOperator
+{
+   public static void main(String args[])
+   {
+    int a=20;
+    int b=10;
+    System.out.print("Addition of a and b :");
+    System.out.println(a+b);
+    System.out.print("Substrction of a and b :");
+    System.out.println(a-b);
+    System.out.print("Multiplication of a and b :");
+    System.out.println(a*b);
+    System.out.print("Division of a and b :");
+    System.out.println(a/b);
+    System.out.print("Modulus of a and b :");
+    System.out.println(a%b);
+   }
+}
