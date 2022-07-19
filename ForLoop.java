1273edd842cdddbceb9e80648c22edf46ff1d3d9
Piyu1913 <109063346+Piyu1913@users.noreply.github.com>
2022-07-19 18:11:10 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+class ForLoop
+{
+   public static void main(String[]args)
+   {
+    System.out.println("Value fron 10 to 6");
+    for(int i=10;i>6;i--)
+      {
+        System.out.println("The value of i:"+ i);
+      }
+   }
+}
