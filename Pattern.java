e098b8d8b5b70b6868f712d5d9c5a181bd49c08c
Piyu1913 <109063346+Piyu1913@users.noreply.github.com>
2022-07-19 16:45:41 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+class Pattern
+{
+	public static void main(String[] ar)
+	{
+		for(int row=1;row<=6;row++)	
+		{
+			for(int column=1;column<=row;column++)	
+			{
+				System.out.print("*"+"  ");
+			}
+			System.out.println();
+		}
+       }
+}
